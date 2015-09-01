ee48f2dde69f42aa3d3572b3dde2301b935eef2a
Spencer Herzberg <spencer.herzberg@gmail.com>
2015-09-01 16:36:14 -0500

    use ishell's error logging
    fixes #4

--- shell.go
+++ shell.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/abiosoft/ishell"
 	"github.com/docker/libkv/store"
@@ -12,32 +13,40 @@ func buildShell(client store.Store) (*ishell.Shell, error) {
 	shell.Println("kvshell starting...")
 
 	shell.Register("ls", func(args ...string) (string, error) {
+		if len(args) != 1 {
+			err := errors.New("Invalid number of args. >> ls path")
+			return "", err
+		}
 		pairs, err := client.List(args[0])
 		if err != nil {
-			panic(err)
-		}
+			return "", err
+		} else {
+			result := ""
+			for _, pair := range pairs {
+				result += fmt.Sprintf("%s => %s", pair.Key, string(pair.Value))
+			}
 
-		result := ""
-		for _, pair := range pairs {
-			result += fmt.Sprintf("%s => %s", pair.Key, string(pair.Value))
+			return result, nil
 		}
-
-		return result, nil
 	})
 
 	shell.Register("get", func(args ...string) (string, error) {
 		pair, err := client.Get(args[0])
 		if err != nil {
-			panic(err)
+			return "", err
 		}
 		result := fmt.Sprintf("%s => %v", pair.Key, string(pair.Value))
 		return result, nil
 	})
 
 	shell.Register("set", func(args ...string) (string, error) {
+		if len(args) != 2 {
+			return "", errors.New("Invalid number of args. >> set path value")
+		}
+
 		err := client.Put(args[0], []byte(args[1]), nil)
 		if err != nil {
-			panic(err)
+			return "", err
 		}
 		result := fmt.Sprintf("%s => %v", args[0], args[1])
 		return result, nil
